56af0cbf69a1d90fefb8c20d7791d1317842df37
JaRutkowski <rutkowskik6@gmail.com>
2019-04-12 20:23:50 +0200

    SKanska #2 Implemmentacja warstwy DAO

---
+++ source/src/controller/utils/com/javafee/common/Constants.java
@@ -0,0 +1,11 @@
+package com.javafee.common;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+	public final String DATA_BASE_PACKAGE_TO_SCAN = "com.javafee.hibernate.dto";
+	public final String DATA_BASE_URL = "127.0.0.1:5432/elibrary";
+	public final String DATA_BASE_USER = "postgres";
+	public final String DATA_BASE_PASSWORD = "admin123";
+}
